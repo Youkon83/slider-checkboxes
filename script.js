381829701654b5fd551ffaf63b931a3e96d748ae
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2023-05-23 15:15:04 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => onlyTwo(e.target)))
+
+function onlyTwo(clicked){
+    if(good.checked && cheap.checked && fast.checked){
+      
+        if(good === clicked){
+            fast.checked = false
+        }
+
+        if(cheap === clicked){
+            good.checked = false
+        }
+
+        if(fast === clicked){
+            cheap.checked = false
+        }
+    }
+}
